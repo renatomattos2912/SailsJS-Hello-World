5698ab88e710e43ccdb62d8df8fbc1c1d266b1b0
renatomattos2912 <renatomattos2912@gmail.com>
2014-09-10 12:58:34 +0000

    teste dokku

--- config/connections.js
+++ config/connections.js
@@ -58,7 +58,7 @@ module.exports.connections = {
   ***************************************************************************/
   mongoLocal: {
     adapter: 'sails-mongo',
-    host: 'mongodb://sails:aXpSVWFHYWlrMXcwRkxKOHNITVhwdFpNSnRUY3owQ05OaitBSnFScjh6dz0K@172.17.0.13:27017/sails-production',
+    host: process.env.MONGO_URL
     //port: 27017,
     // user: 'username',
     // password: 'password',
